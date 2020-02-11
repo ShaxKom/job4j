831ce96a0c23f1828df22913c918bb3b609e2cd0
ShaxKom <shax.kom@mail.ru>
2020-02-11 23:38:34 +0600

    Максимум из двух чисел[#234175]

---
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class MaxTest {
+    @Test
+    public void whenMax1to2Then2(){
+        int result = Max.max(1,2);
+        assertThat(result,is(2));
+    }
+
+    @Test
+    public void whenMax1to2Then1(){
+        int result = Max.max(51,23);
+        assertThat(result,is(51));
+    }
+
+    @Test
+    public void whenMax1to2Equal(){
+        int result = Max.max(14,14);
+        assertThat(result,is(14));
+    }
+
+}
