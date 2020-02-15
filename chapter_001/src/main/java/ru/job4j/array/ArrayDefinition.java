c6bd9f129eadf8726ae124552685cd89510adadd
ShaxKom <shax.kom@mail.ru>
2020-02-15 20:29:49 +0600

    Заполнение массива.[#234195]

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,12 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0]= "Petr Arsentev";
+        names[1]= "Shakhzod Kamilov";
+        names[2]= "Ilon Musk";
+        names[3]= "Bill Gates";
+        for (int i=0; i<=3;i++)
+        System.out.println(names[i]);
     }
 }
