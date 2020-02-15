5f13fb19b119ea2ba4052331fbcaddbc709377fa
ShaxKom <shax.kom@mail.ru>
2020-02-15 20:48:41 +0600

    Массивы и цикл for.[#234196]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayLoop.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+/**
+ * Класс для записи данных в массив с помощью цикла
+ * @author Shakhzod Kamilov
+ */
+
+public class ArrayLoop {
+    /**
+     * Main.
+     * @param args-args
+     */
+    public static void main(String[] args){
+        int[] numbers = new int[5];
+        for (int index=0; index<numbers.length;index++){
+            numbers[index]= index*2+3;
+        }
+        for (int number : numbers) System.out.println(number);
+    }
+}
