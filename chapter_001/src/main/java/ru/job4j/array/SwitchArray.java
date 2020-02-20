1091b0cb0f83263b466865f05a1914c1fb64d65e
ShaxKom <shax.kom@mail.ru>
2020-02-20 18:56:18 +0600

    Переставить элементы массива[#234200]

--- chapter_001/src/main/java/ru/job4j/array/SwitchArray.java
+++ chapter_001/src/main/java/ru/job4j/array/SwitchArray.java
@@ -1,6 +1,30 @@
 package ru.job4j.array;
 
+/**
+ * Класс для перестановки элементов массива
+ * @author Shakhzod Kamilov
+ * @version 2
+ */
 public class SwitchArray {
+    /**
+     * Метод для перестановки произволных элементов заданного массива
+     * @param array Входной массив
+     * @param source первый элемент перестановки
+     * @param dest второй элемент для перестановки
+     * @return результат перестановки, возвращает массив
+     */
+    public static int[] swap(int[] array,int source, int dest ){
+        int temp = array[source];
+        array[source]=array[dest];
+        array[dest]=temp;
+        return array;
+    }
+
+    /**
+     * Метод для перестановки первого и последнего элемента массива
+     * @param array Входной массив
+     * @return результат перестановки, возвращает массив
+     */
     public static int[] swapBorder(int[] array) {
         int temp = array[0];
         array[0] = array[array.length-1];
@@ -8,11 +32,15 @@ public class SwitchArray {
         return array;
     }
 
+    /**
+     * Main
+     * @param args - args
+     */
     public static void main(String[] args) {
         int[] nums = new int[] {1, 2, 3, 4, 5, 6};
         int[] rsl = swapBorder(nums);
-        for (int index = 0; index < rsl.length; index++) {
-            System.out.println(rsl[index]);
+        for (int i : rsl) {
+            System.out.println(i);
         }
     }
 }
---
+++ chapter_001/src/test/java/ru/job4j/array/SwitchArrayTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class SwitchArrayTest {
+    @Test
+    public void whenSwap1and3() {
+        int[] input = new int[]{1, 4, 3, 2, 5};
+        int[] expect = new int[]{1, 2, 3, 4, 5};
+        int[] rst = SwitchArray.swap(input, 1, 3);
+        assertThat(rst,is(expect));
+    }
+
+    @Test
+    public void whenSwap4and9(){
+        int[] expect = new int[]{0,1,2,3,4,5,6,7,8,9};
+        int[] input = new int[]{0,1,2,3,9,5,6,7,8,4};
+        int[] rst = SwitchArray.swap(input,4,input.length-1);
+        assertThat(rst,is(expect));
+    }
+
+}
