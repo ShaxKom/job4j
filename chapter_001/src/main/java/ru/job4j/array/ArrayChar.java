653a1c91cd728c36501a7866b5ef8a56d3e792b0
ShaxKom <shax.kom@mail.ru>
2020-02-24 22:41:29 +0600

    Слово начинается с ...[#234183]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref){
+        boolean result = false;
+        if(word.length>=pref.length) {
+            result = true;
+            for (int index = 0; index < pref.length; index++) {
+                if (word[index] != pref[index]) {
+                    result = false;
+                    break;
+                }
+
+            }
+        }
+        return result;
+    }
+}
