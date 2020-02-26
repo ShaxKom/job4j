43545a8e16bfbafe6b443fab700c061fe6019688
ShaxKom <shax.kom@mail.ru>
2020-02-26 13:35:29 +0600

    Слово заканчивается с ...[#234187]

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+/**
+ * Класс для определения на совпадение последних символов массива
+ * @author Shakhzod Kamilov
+ * @version 1
+ */
+public class EndsWith {
+    /**
+     * Определяет совпадают ли последние символы двух массивов
+     * @param word - слово целиком
+     * @param post - постфикс
+     * @return возвращает логическое вырежение.
+     */
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = false;
+        if (word.length >= post.length) {
+            result = true;
+            for (int index=1;index<post.length;index++){
+                if(word[word.length-index]!=post[post.length-index]){
+                    result=false;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/EndsWithTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Тест-класс для EndsWith.java
+ */
+public class EndsWithTest {
+
+    @Test
+    public void whenEndWithPostThenTrue(){
+        char[] word = {'H','e','l','l','o'};
+        char[] post = {'l','l','o'};
+        boolean result = EndsWith.endsWith(word,post);
+        assertThat(result,is(true));
+    }
+
+    @Test
+    public void whenNotEndWithPostThenFalse(){
+        char[] word = {'H','e','l','l','o'};
+        char[] post = {'l','l','e'};
+        boolean result = EndsWith.endsWith(word,post);
+        assertThat(result,is(false));
+    }
+
+}
