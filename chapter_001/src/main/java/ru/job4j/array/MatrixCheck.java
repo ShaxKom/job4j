3d7932a3cf255321e2548afea3fdb1a7bb73b8e6
ShaxKom <shax.kom@mail.ru>
2020-03-02 18:25:55 +0600

    Моно столбец в матрице.[#234193]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -1,15 +1,39 @@
 package ru.job4j.array;
 
 /**
- * Проверяет заполнена ли определённая строка массива символом 'X'
+ * Проверяет заполнена ли определённая строка или столбец матрицы символом 'X'
  * @author Shakhzod Kamilov
+ * @version 2
  */
 
 public class MatrixCheck {
+    /**
+     * Проверяет строки
+     * @param board входная матрица
+     * @param row строка для проверки
+     * @return возвращает логическое выражение
+     */
     public static boolean monoHorizontal(char[][] board, int row){
         boolean result = true;
-        for(int i=0;i<board.length;i++){
+        for(int i=0;i<board[row].length;i++){
             if (board[row][i]!='X'){
+                result=false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Проверяет столбцы
+     * @param board входная матрица
+     * @param colomn столбец для проверки
+     * @return возвращает логическое вырадение
+     */
+    public static boolean monoVertical(char[][] board, int colomn){
+        boolean result = true;
+        for (int i=0;i<board.length;i++){
+            if(board[i][colomn]!='X'){
                 result=false;
                 break;
             }
--- chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -4,12 +4,20 @@ import org.junit.Test;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 
+/**
+ * Тетс-класс для MatrixCheck.java
+ * @author Shakhzod Kamilov
+ * @version 2
+ */
+
 public class MatrixCheckTest {
     @Test
     public void whenHasMonoHorizontal(){
         char[][] input = {
                 {'X',' ',' ',' '},
                 {'X','X','X','X'},
+                {'X',' ',' ',' '},
+                {'X',' ',' ',' '},
                 {'X',' ',' ',' '}
         };
         boolean result = MatrixCheck.monoHorizontal(input,1);
@@ -19,10 +27,34 @@ public class MatrixCheckTest {
     public void whenHasNotMonoHorizontal(){
         char[][] input = {
                 {'X','X','X','X'},
-                {'X',' ','X','X'},
-                {'X',' ',' ',' '}
+                {' ',' ','X','X'},
+                {'X',' ',' ',' '},
+                {'X',' ',' ',' '},
+                {' ',' ',' ',' '}
         };
         boolean result = MatrixCheck.monoHorizontal(input,1);
         assertThat(result,is(false));
+    }
+    @Test
+    public void whenHasMonoVertical(){
+        char[][] input={
+                {' ','X',' ','X',' '},
+                {' ','X',' ','X',' '},
+                {' ',' ',' ','X',' '},
+                {' ',' ',' ','X',' '} };
+        boolean result = MatrixCheck.monoVertical(input,3);
+        assertThat(result,is(true));
+    }
+
+    @Test
+    public void whenHasNotMonoVertical(){
+        char[][] input = {
+                {' ',' ',' ','X'},
+                {' ',' ',' ','X'},
+                {' ',' ',' ',' '},
+                {' ',' ',' ','X'} };
+        boolean result = MatrixCheck.monoVertical(input,3);
+        assertThat(result,is(false));
+
     }
 }
