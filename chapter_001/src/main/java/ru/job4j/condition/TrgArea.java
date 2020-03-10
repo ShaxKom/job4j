655a090000a8f41a8d627dea2f4d18c1a0dbe57d
ShaxKom <shax.kom@mail.ru>
2020-03-10 18:19:13 +0600

    Рефакторинг - Площадь треугольника.[#234346]

--- chapter_001/src/main/java/ru/job4j/condition/TrgArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/TrgArea.java
@@ -1,15 +1,48 @@
 package ru.job4j.condition;
 
+/**
+ * Расчёты с тругольником
+ * @author Shakhzod Kamilov
+ * @version 2
+ */
 public class TrgArea {
-    public static double area(double a, double b, double c) {
-        double p = (a + b + c) / 2;
-        double rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-        return rsl;
+    private Point first;
+    private Point second;
+    private Point third;
+
+    /**
+     * Инициализирует поля
+     * @param ap Координаты первой точки
+     * @param bp Координаты второй точки
+     * @param cp Координаты третьей точки
+     */
+    public TrgArea(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
     }
-    public static void main(String[] args) {
-        double rsl = TrgArea.area(2, 2, 2);
-        System.out.println("area (2,2,2) = " + rsl);
-        rsl = TrgArea.area(2, 3, 4);
-        System.out.println("area (2,3,4) = " + rsl);
+
+    /**
+     * Периметр треуголника по заданным координатам точек
+     * @return возвращает периметр
+     */
+    public double period() {
+        return (first.distance(second) + second.distance(third) + third.distance(first)) / 2;
+    }
+
+    /**
+     * Вычесляет площадь треугольника по заданным координатам точек
+     * @return возвращает площадь или -1 в случае не существования треуголника
+     */
+    public double area() {
+        double p = period();
+        double a = first.distance(second);
+        double b = second.distance(third);
+        double c = third.distance(first);
+        double rsl = -1;
+        if (Triangle.exist(a, b, c)) {
+            rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        return rsl;
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,8 +1,13 @@
 package ru.job4j.condition;
 import org.junit.Test;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.is;
 
+/**
+ * Тест-класс для Triangle.java и TrgArea.java
+ * @author Shakhzod Kamilov
+ * @version 2
+ */
 public class TriangleTest {
     @Test
     public void whenExist() {
@@ -15,4 +20,29 @@ public class TriangleTest {
         assertThat(result, is(false));
     }
 
+    @Test
+    public void triangleArea() {
+        Point a1 = new Point(1, 1);
+        Point b1 = new Point(2, 5);
+        Point c1 = new Point(5, 3);
+        TrgArea trg = new TrgArea(a1, b1, c1);
+        assertEquals(7, trg.area(), 0.01);
+    }
+    @Test
+    public void triangleArea1() {
+        Point a1 = new Point(0, 0);
+        Point b1 = new Point(3, 2);
+        Point c1 = new Point(7, 5);
+        TrgArea trg = new TrgArea(a1, b1, c1);
+        assertEquals(0.5, trg.area(), 0.01);
+    }
+
+    @Test
+    public void triangleAreaWhenDoesNotExist() {
+        Point a1 = new Point(0, 2);
+        Point b1 = new Point(2, 4);
+        Point c1 = new Point(4, 6);
+        TrgArea trg = new TrgArea(a1, b1, c1);
+        assertEquals(-1, trg.area(), 0.01);
+    }
 }
