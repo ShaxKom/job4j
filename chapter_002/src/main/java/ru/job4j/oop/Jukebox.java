a14324f8cd25935e2addb46e6fbec58124c7abfa
ShaxKom <shax.kom@mail.ru>
2020-03-10 16:30:13 +0600

    Вызов метода с аргументами.[#234352]

--- chapter_002/src/main/java/ru/job4j/oop/Jukebox.java
+++ chapter_002/src/main/java/ru/job4j/oop/Jukebox.java
@@ -3,20 +3,20 @@ package ru.job4j.oop;
 public class Jukebox {
     public void music(int position) {
         if (position == 1) {
-            System.out.println("Пуст бегут неуклюже\n"
-                    + "Пешеходы по лужам,\n"
-                    + "А вода — по асфальту рекой.\n"
-                    + "И неясно прохожим\n"
-                    + "В этот день непогожий,\n"
-                    + "Почему я веселый такой.\n"
+            System.out.println("Пуст бегут неуклюже" + System.lineSeparator()
+                    + "Пешеходы по лужам," + System.lineSeparator()
+                    + "А вода — по асфальту рекой." + System.lineSeparator()
+                    + "И неясно прохожим" + System.lineSeparator()
+                    + "В этот день непогожий," + System.lineSeparator()
+                    + "Почему я веселый такой." + System.lineSeparator()
                     + "");
         } else if (position == 2) {
-            System.out.println("Спят усталые игрушки, книжки спят.\n"
-                    + "Одеяла и подушки ждут ребят.\n"
-                    + "Даже сказка спать ложится,\n"
-                    + "Чтобы ночью нам присниться.\n"
-                    + "Ты ей пожелай:\n"
-                    + "Баю-бай.\n"
+            System.out.println("Спят усталые игрушки, книжки спят." + System.lineSeparator()
+                    + "Одеяла и подушки ждут ребят." + System.lineSeparator()
+                    + "Даже сказка спать ложится," + System.lineSeparator()
+                    + "Чтобы ночью нам присниться." + System.lineSeparator()
+                    + "Ты ей пожелай:" + System.lineSeparator()
+                    + "Баю-бай." + System.lineSeparator()
                     + "");
         } else {
             System.out.println("Песня не найдена");
