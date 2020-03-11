db074782dcea1ae10d4e3c840e7ef8104b879481
ShaxKom <shax.kom@mail.ru>
2020-03-11 16:19:44 +0600

    Расстояние между точками в трехмерном пространстве.[#234347]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -2,23 +2,53 @@ package ru.job4j.condition;
 import static java.lang.Math.sqrt;
 import static java.lang.Math.pow;
 
+/**
+ * Нахождение расстояния между точками
+ */
 public class Point {
+    /**
+     * Поля объекта, координаты точек
+     */
     private int x;
     private int y;
+    private int z;
 
-    public Point(int first, int second) {
-        this.x = first;
-        this.y = second;
+    /**
+     * Конструктор, принимает начальное состояние
+     * @param x координата х
+     * @param y координата у
+     */
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
+    /**
+     * Конструктор, принимает начальное состояние
+     * @param x координата х
+     * @param y координата у
+     * @param z координата z
+     */
+    public Point(int x, int y, int z) {
+        this(x, y);
+        this.z = z;
+    }
+
+    /**
+     * Находит расстояние между точками на плоскости
+     * @param that принимает другой объект Point
+     * @return возвращает расстояние
+     */
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
-    public static void main(String[] args) {
-        Point a = new Point(0, 0);
-        Point b = new Point(0, 2);
-        double dist = a.distance(b);
-        System.out.println(dist);
+    /**
+     * Находит расстояние между точками в пространстве
+     * @param that принимает другой объект Point
+     * @return возвращает расстояние
+     */
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -3,6 +3,9 @@ package ru.job4j.condition;
 import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * Тест-класс для Point.java
+ */
 public class PointTest {
 
     @Test
@@ -12,5 +15,23 @@ public class PointTest {
         double expected = 14.14;
         double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void distance3d() {
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(3, 3, 3);
+        double expected = 5.19;
+        double out = a.distance3d(b);
+        Assert.assertEquals(out, expected, 0.01);
+    }
+
+    @Test
+    public void distance3d2() {
+        Point a = new Point(1, 1, 1);
+        Point b = new Point(6, 6, 6);
+        double expected = 8.66;
+        double out = a.distance3d(b);
+        Assert.assertEquals(out, expected, 0.01);
     }
 }
