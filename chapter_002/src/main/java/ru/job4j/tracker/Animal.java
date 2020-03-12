730b45ad4f794ec6add90e93d9d49ee57dd36342
ShaxKom <shax.kom@mail.ru>
2020-03-12 23:33:55 +0600

    Перегрузка конструктора.[#234361]

--- chapter_002/src/main/java/ru/job4j/tracker/Animal.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Animal.java
@@ -1,7 +1,12 @@
 package ru.job4j.tracker;
 
 public class Animal {
+    String name;
     public Animal() {
         System.out.println("Animal");
+    }
+
+    public Animal(String name) {
+        this.name = name;
     }
 }
--- chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
@@ -5,6 +5,10 @@ public class Tiger extends Predator {
         System.out.println("Tiger");
     }
 
+    public Tiger(String name) {
+        super(name);
+    }
+
     public static void main(String[] args) {
         Tiger tiger = new Tiger();
     }
