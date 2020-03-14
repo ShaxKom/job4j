32de1c13685c09a4ed853766ff0b99cdbc360f09
ShaxKom <shax.kom@mail.ru>
2020-03-14 18:44:55 +0600

    Аннотация @Override[#234363]

--- chapter_002/src/main/java/ru/job4j/inheritance/JSONReport.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,6 +2,8 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
     private static final String LN = System.lineSeparator();
+
+    @Override
     public String generate(String name, String body) {
         return "{" + LN
                 + "name : " + name + LN
