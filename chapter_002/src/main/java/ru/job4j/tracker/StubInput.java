5e0623c7883b72bdc7a870f82dd24ead8308758d
ShaxKom <shax.kom@mail.ru>
2020-05-01 17:38:11 +0600

    Тестирование. Подготовка данных. [#234326]

--- chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -2,9 +2,9 @@ package ru.job4j.tracker;
 
 public class StubInput implements Input {
     private String[] answer;
-    private int position =0;
+    private int position = 0;
 
-    public StubInput(String[] answer){
+    public StubInput(String[] answer) {
         this.answer = answer;
     }
 
--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,6 +1,8 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.is;
 
@@ -14,5 +16,26 @@ public class StartUITest {
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
+    }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Item");
+        tracker.add(item);
+        String[] answer = {item.getId(), "Replaced"};
+        StartUI.replaceItem(new StubInput(answer), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("Replaced"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Item");
+        tracker.add(item);
+        String[] answer = {item.getId()};
+        StartUI.deleteItem(new StubInput(answer), tracker);
+        assertThat(tracker.findById(item.getId()), is(nullValue()));
     }
 }
