365af98abe296749738d85030eae689bea44b01f
ShaxKom <shax.kom@mail.ru>
2020-08-08 19:50:42 +0600

    Рефакторинг - Шаблон Декоратор для валидатора.

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -155,4 +155,31 @@ public class StartUITest {
                 + "Menu.%n"
                 + "0. Exit program%n")));
     }
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[] {"one", "1"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu: ");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[] {"0"});
+        ValidateInput input = new ValidateInput(out, in);
+        assertThat(input.askInt("Enter: "), is(0));
+    }
+
+    @Test
+    public void outputCheck() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[] {"in", "2"});
+        ValidateInput input = new ValidateInput(out, in);
+        input.askInt("Enter");
+        assertThat(out.toString(), is(String.format("You can enter only numbers!%n")));
+    }
+
 }
